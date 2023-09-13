3bdaef829904ec0812fc1b2a37c43f07d917539c
Erich <erichboerr@hotmail.com>
2023-09-13 15:14:56 -0300

    Proyecto Preparado para iniciar, carpetas creadas

---
+++ src/content/assets/page/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav>
+            <h1>Header</h1>
+        </nav>
+    );
+}
+
+export default Header;
---
+++ src/content/assets/page/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <main>
+            <h1>Pagina Principal</h1>
+        </main>
+    );
+}
+
+export default Main;
