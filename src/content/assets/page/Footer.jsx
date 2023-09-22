53f2da5308717db4481c8909c04ba358f90dbfad
Patricio Prieto Garay <93171508+patricioprietogaray@users.noreply.github.com>
2023-09-22 14:52:27 -0300

    footer simple
    creacion de un footer simple

--- src/content/assets/page/Footer.jsx
+++ src/content/assets/page/Footer.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import '../css/Footer.css';
+import logo from "../image/logo.png";
 
 
 const Footer = () => {
     return (
-        <footer>
-            <h1>Footer</h1>
+        <footer id='pie'>
+            <img src={logo} id='logoFooter'/>
+            <ul id='menuPie'>
+                <li><a href='#'>Item1</a></li>
+                <li><a href='#'>Item2</a></li>
+                <li><a href='#'>Item3</a></li>
+                <li><a href='#'>Item4</a></li>
+            </ul>
+            <h3 id='contact'>Contact Us</h3>
         </footer>
     );
 }
