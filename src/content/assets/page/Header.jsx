fa2009e90403e0d0198d4fadffffa4ad13a5b033
Luckyllas <lucasezequielvera@hotmail.com>
2023-09-23 11:02:35 -0300

    add: 1st Step Header

--- src/content/assets/page/Header.jsx
+++ src/content/assets/page/Header.jsx
@@ -1,34 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/Header.css";
 import logo from "../image/logo.png";
-import bolso from "../image/bolsoCompras.png";
 
 const Header = () => {
+
+  const [estado, setEstado] = useState("");
+
+  const activarNavbarHandler = () => setEstado("Activado");
+  const desactivarNavbarHandler = () => setEstado("Desactivado");
+
   return (
     <header>
-      <nav className="top-bar">
-        <div className="header-container justify-center">
-          <div className="menu-logo">
-            <div className="logo">
-              <img src={logo} alt="logo" />
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div> {estado}
+        <nav>
+          <button className="abrir-navbar" id="abrir" onClick={activarNavbarHandler}>Abrir</button>
+          <div className="navbar-container" id="navbar-container">
+            <button className="cerrar-navbar" id="cerrar" onClick={desactivarNavbarHandler}>Cerrar</button>
+            <div className="left-navbar">
+              <div>Inicio</div>
+              <div>Productos</div>
+              <div>Quienes Somos</div>
+              <div>Contacto</div>
+            </div>
+            <div className="right-navbar">
+              <div>Iniciar Sesion</div>
+              <div>Registrarse</div>
             </div>
           </div>
-        </div>
-        <div className="header-container space-between">
-          <a href="/carrito">
-            <div className="carrito">
-              <div className="carrito-item-cont">
-                <img
-                  src={bolso}
-                  alt="bolso compras"
-                  className="carrito-compras"
-                />
-                <span className="cont">0</span>
-              </div>
-            </div>
-          </a>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </header>
   );
 };
