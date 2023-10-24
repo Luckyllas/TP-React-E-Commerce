5c1481649cb4a7bc94f1b951d0e3dda442220b13
Erich <erichboerr@hotmail.com>
2023-10-24 09:03:20 -0300

    agregue los destacados al main

--- src/assets/components/DestacadoItem.jsx
+++ src/assets/components/DestacadoItem.jsx
@@ -3,35 +3,30 @@ import "../css/Product.css";
 import { imageProduct } from "../helpers/ImagesDirectory";
 
 const Product = ({ product, addToCart }) => {
-  const { id, img, precio, etiqueta, destacado } = product;
-  
+  const { id, img, precio, etiqueta } = product;
+
   return (
-    <Fragment key={id}>      
-      <div className=" col-auto">
+    <Fragment key={id}>
+      <div className="col w-200">
         <div className="card">
           <img
             src={imageProduct(`./${img}`)}
             alt={etiqueta}
-            className="card-image"
+            className="card-img-top"
           />
-          <figcaption>
-            <ul className="card-data">
-              <li>
-                <span className="card-data-titulo">{etiqueta}</span>
-              </li>
-              <li>
-                <span className="card-data-precio">$ {precio}</span>
-              </li>
-              <li>
-                <button
-                  onClick={() => addToCart(id)}
-                  className="card-button-compra"
-                >
-                  Agregar al Carrito
-                </button>
-              </li>
-            </ul>
-          </figcaption>
+          <div className="card-body w-100">
+            <p className="card-title">{etiqueta}</p>
+          </div>
+          <div className="card-body w-80 row align-items-center">
+            <h4 className="card-text col">$ {precio}</h4>
+            <button
+              type="button"
+              className="btn btn-outline-light btn-lg col-3 pb-3"
+              onClick={() => addToCart(id)}
+            >
+              <i className="bi bi-cart"></i>
+            </button>
+          </div>
         </div>
       </div>
     </Fragment>
